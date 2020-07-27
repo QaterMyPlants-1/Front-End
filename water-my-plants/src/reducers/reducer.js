03840ca3d0c19bfa124836de36bf57614e64a68c
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-07-27 15:24:04 -0700

    Create draft of editPlants action to update Plant objects

--- water-my-plants/src/reducers/reducer.js
+++ water-my-plants/src/reducers/reducer.js
@@ -26,6 +26,24 @@ export const reducer = (state = initialState, action) => {
         isLoading: false,
         error: action.payload,
       };
+    case actions.ADD_PLANTS_START:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case actions.ADD_PLANT_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        plants: [...state.plants, action.payload],
+        error: "",
+      };
+    case actions.ADD_PLANT_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
